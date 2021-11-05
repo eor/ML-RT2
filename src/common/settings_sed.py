e84cb2114d1e773a9ad4c3cb898802a432926f59
eor <eor@users.noreply.github.com>
2021-11-05 23:03:50 +0000

    renamed file, added settings

--- src/common/settings_sed.py
+++ src/common/settings_sed.py
@@ -1,35 +1,19 @@
 # -----------------------------------------------------------------
-#  settings for dataset with 5 parameters
+#  Settings for the input SEDs
 # -----------------------------------------------------------------
-# Parameter names and their respective intervals
-# 1. haloMassLog        interval=[8.0, 15.0]
-# 2. redshift           interval=[6.0, 13.0]
-# 3. sourceAge          interval=[0.1, 20.0]
-# 4. qsoAlpha           interval=[1.0, 2.0]
-# 5. starsEscFrac       interval=[0.0, 1.0]
 
-
-p5_limits = list()
-p5_limits.append([8.0, 15.0])
-p5_limits.append([6.0, 13.0])
-p5_limits.append([0.1, 20.0])
-p5_limits.append([1.0, 2.0])
-p5_limits.append([0.0, 1.0])
-
-
-p5_names_latex = list()
-p5_names_latex.append('\log_{10}{\mathrm{M}_\mathrm{halo}}')
-p5_names_latex.append('z')
-p5_names_latex.append('t_{\mathrm{source}}')
-p5_names_latex.append('-\\alpha_{\mathrm{QSO}}')
-p5_names_latex.append('f_{\mathrm{esc,\\ast}}')
-
-
-
-
+SED_ENERGY_MIN = 1.0    # eV
+SED_ENERGY_MAX = 1e4    # eV
+SED_ENERGY_DELTA = 0.1  # eV (resolution)
 
 # -----------------------------------------------------------------
-#  settings for dataset with 8 parameters
+#  Settings for the SED sampling of the p-space?
+# -----------------------------------------------------------------
+# ...
+
+# -----------------------------------------------------------------
+#  Settings for SED generation with 8 parameters, they are
+#  same as in the first ML-RT project
 # -----------------------------------------------------------------
 #        1. haloMassLog         interval=[8.0, 15.0]
 #        2. redshift            interval=[6.0, 13.0]
@@ -59,3 +43,31 @@ p8_names_latex.append('\\epsilon_{\mathrm{QSO}}')
 p8_names_latex.append('f_{\mathrm{esc,\\ast}}')
 p8_names_latex.append('\\alpha_{\mathrm{IMF,\\ast}}')
 p8_names_latex.append('\log_{10}{\mathrm{M}_{\mathrm{min}, \\ast}}')
+
+
+# -----------------------------------------------------------------
+#  Settings for SED generation with 5 parameters, they are
+#  same as in the first ML-RT project
+# -----------------------------------------------------------------
+# Parameter names and their respective intervals
+# 1. haloMassLog        interval=[8.0, 15.0]
+# 2. redshift           interval=[6.0, 13.0]
+# 3. sourceAge          interval=[0.1, 20.0]
+# 4. qsoAlpha           interval=[1.0, 2.0]
+# 5. starsEscFrac       interval=[0.0, 1.0]
+
+
+p5_limits = list()
+p5_limits.append([8.0, 15.0])
+p5_limits.append([6.0, 13.0])
+p5_limits.append([0.1, 20.0])
+p5_limits.append([1.0, 2.0])
+p5_limits.append([0.0, 1.0])
+
+
+p5_names_latex = list()
+p5_names_latex.append('\log_{10}{\mathrm{M}_\mathrm{halo}}')
+p5_names_latex.append('z')
+p5_names_latex.append('t_{\mathrm{source}}')
+p5_names_latex.append('-\\alpha_{\mathrm{QSO}}')
+p5_names_latex.append('f_{\mathrm{esc,\\ast}}')
