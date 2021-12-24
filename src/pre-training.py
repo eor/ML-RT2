31bc4e13d3f1929bea17ad16390db1b036f0f620
Aayush Singla <aayushsingla1729@gmail.com>
2021-12-25 01:58:57 +0530

    added model and pretraining procedure

--- src/pre-training.py
+++ src/pre-training.py
@@ -18,7 +18,7 @@ from common.settings_crt import *
 from common.settings import *
 from common.data_log import *
 from sed import sed_numba
-from models import *
+from models_pretraining import *
 from random import random
 
 # check for CUDA
@@ -45,6 +45,7 @@ def main(config):
     else:
         run_id = 'run_' + config.run
     config.out_dir = os.path.join(config.out_dir, run_id)
+    setattr(config, 'run', run_id)
 
     utils_create_output_dirs([config.out_dir])
     utils_save_config_to_log(config)
@@ -77,22 +78,22 @@ def main(config):
         flux_vectors = np.log10(flux_vectors + 1.0e-6)
 
     # -----------------------------------------------------------------
-    # convert data into dataset and split it into requried legths
+    # convert data into tensors and split it into requried legths
     # -----------------------------------------------------------------
+
     # numpy array to tensors
     flux_vectors = torch.Tensor(flux_vectors)
-    # dataset from numpy arrays
-    dataset = torch_data.TensorDataset(flux_vectors)
+
     # calculate length for train. val and test dataset from fractions
     train_length = int(PRETRAINING_SPLIT_FRACTION[0] * config.n_samples)
     validation_length = int(PRETRAINING_SPLIT_FRACTION[1] * config.n_samples)
     test_length = config.n_samples - train_length - validation_length
+
     # split the dataset
     train_dataset, validation_dataset, test_dataset = \
-     torch.utils.data.random_split(dataset,
+     torch.utils.data.random_split(flux_vectors,
                     (train_length, validation_length,test_length),
                     generator=torch.Generator().manual_seed(PRETRAINING_SEED))
-
 
     # -----------------------------------------------------------------
     # dataloaders from dataset
@@ -107,14 +108,63 @@ def main(config):
     # data_log = DataLog.getInstance(config.out_dir)
     # data_log.start_server()
 
+    # -----------------------------------------------------------------
+    # initialise model
+    # -----------------------------------------------------------------
+    model = AE1(config)
+    print('\n\tusing model AE1 on device: %s\n'%(device))
+
+    if cuda:
+        model.cuda()
+
+    # -----------------------------------------------------------------
+    # Optimizers
+    # -----------------------------------------------------------------
+    optimizer = torch.optim.Adam(
+        model.parameters(),
+        lr=config.lr,
+        betas=(config.b1, config.b2)
+    )
+
+
+    # -----------------------------------------------------------------
+    # book keeping arrays
+    # -----------------------------------------------------------------
+    avg_train_loss_array = np.empty(0)
+    avg_val_loss_mse_array = np.empty(0)
 
     # -----------------------------------------------------------------
     #  Main training loop
     # -----------------------------------------------------------------
     print("\033[96m\033[1m\nTraining starts now\033[0m")
     for epoch in range(1, config.n_epochs + 1):
-        for i, (flux_vectors) in enumerate(train_loader):
+        epoch_loss = 0
+        # set model mode
+        model.train()
+        for i, flux_vectors in enumerate(train_loader):
             # train the model here.
+            flux_vectors = Variable(flux_vectors)
+
+            # zero the gradients on each iteration
+            optimizer.zero_grad()
+            regen_flux_vectors = model(flux_vectors)
+
+            # compute loss
+            loss = F.mse_loss(input=regen_flux_vectors, target=flux_vectors, reduction='mean')
+            loss.backward()
+            optimizer.step()
+
+            # sum the loss values
+            epoch_loss += loss.item()
+
+        # end-of-epoch book keeping
+        train_loss = epoch_loss / len(train_loader)
+        avg_train_loss_array = np.append(avg_train_loss_array, train_loss)
+
+        # [TODO] validation
+
+        print("[Epoch %d/%d] [Train loss: %e] [Validation loss: %e][Best_epoch: %d]"
+         % (epoch, config.n_epochs, train_loss, 0, 0))
 
 
     # -----------------------------------------------------------------
@@ -134,15 +184,6 @@ if __name__ == "__main__":
                         help='Path of the data directory from which data is to be read for training the model, default: ../data/sed_samples')
     parser.add_argument('--run', type=str, default='', metavar='(string)',
                         help='Specific run name for the experiment, default: ./output/timestamp')
-
-    parser.add_argument("--len_SED_input", type=int, default=2000,
-                        help="length of SED input for the model")
-    parser.add_argument("--len_latent_vector", type=int, default=8,
-                        help="length of reduced SED vector")
-    parser.add_argument("--len_state_vector", type=int, default=5,
-                        help="length of state vector (Xi, T, t) to be concatenated with latent_vector")
-    parser.add_argument("--train_set_size", type=int, default=512,
-                        help="size of the randomly generated training set (default=512)")
 
     # grid settings
     parser.add_argument("--radius_max", type=float, default=DEFAULT_RADIUS_MAX,
@@ -171,7 +212,7 @@ if __name__ == "__main__":
                         help="adam: beta2 - decay of first order momentum of gradient, default=0.999")
 
     # model configuration
-    parser.add_argument("--model", type=str, default='MLP1', help="Model to use")
+    parser.add_argument("--model", type=str, default='AE1', help="Model to use")
     parser.add_argument("--batch_norm", dest='batch_norm', action='store_true',
                         help="use batch normalisation in network (default)")
     parser.add_argument('--no-batch_norm', dest='batch_norm', action='store_false',
@@ -184,6 +225,9 @@ if __name__ == "__main__":
                         help="do not use dropout regularisation in network")
     parser.set_defaults(dropout=False)
     parser.add_argument("--dropout_value", type=float, default=0.25, help="dropout probability, default=0.25 ")
+
+    parser.add_argument("--len_latent_vector", type=int, default=8,
+                        help="length of reduced SED vector")
 
     my_config = parser.parse_args()
 
